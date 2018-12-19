9f2bf74d264c43323c9acdf1c2f9424c00506448
Omar Younis <o.younis@pitechnologies.net>
2018-12-19 11:32:59 +0300

    updated webhook script

--- license_portal/helpers.py
+++ license_portal/helpers.py
@@ -4,11 +4,12 @@ import os
 from django.http.response import HttpResponseServerError, HttpResponse
 
 
-PULL_BATCH_PATH = os.path.join('C:\\', 'Apache24-general', 'htdocs', 'django_apps', 'license_portal', 'license_portal', 'gitpull_windows.bat')
+PULL_BATCH_FILE = 'gitpull_windows.bat'
+PULL_BATCH_DIR = os.path.join('C:\\', 'Apache24-general', 'htdocs', 'django_apps', 'license_portal', 'license_portal')
 
 
 def gitwebhooks(request):
-    proc = subprocess.Popen([PULL_BATCH_PATH], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen([PULL_BATCH_FILE], cwd=PULL_BATCH_DIR, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     res, err = proc.communicate()
 
