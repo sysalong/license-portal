822d5e6356d1d4ee8317fa71dfbd20f2c6d857c8
Omar Younis <o.younis@pitechnologies.net>
2018-12-19 11:58:28 +0300

    test

--- license_portal/helpers.py
+++ license_portal/helpers.py
@@ -4,13 +4,13 @@ import os
 from django.http.response import HttpResponseServerError, HttpResponse
 
 
-PULL_BATCH_FILE = 'gitpull_windows.bat'
+PULL_BATCH_FILE = r'gitpull_windows.bat'
 PULL_BATCH_DIR = os.path.join('C:\\', 'Apache24-general', 'htdocs', 'django_apps', 'license_portal', 'license_portal')
 
 
 def gitwebhooks(request):
     try:
-        proc = subprocess.check_output(['mkdir', 'test'], cwd=PULL_BATCH_DIR)
+        proc = subprocess.check_output(['mkdir', 'test'], cwd=PULL_BATCH_DIR, shell=True)
     except Exception as e:
         return HttpResponseServerError(e)
 
