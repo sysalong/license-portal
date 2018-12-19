167136aadae9e279dc08e77890169dd7a9db842c
Omar Younis <o.younis@pitechnologies.net>
2018-12-19 12:17:26 +0300

    update

--- license_portal/helpers.py
+++ license_portal/helpers.py
@@ -8,21 +8,23 @@ from django.views.decorators.csrf import csrf_exempt
 PULL_BATCH_FILE = r'gitpull_windows.bat'
 PULL_BATCH_DIR = os.path.join('C:\\', 'Apache24-general', 'htdocs', 'django_apps', 'license_portal', 'license_portal')
 
+BRANCH_REF = 'refs/heads/staging'
+
 
 @csrf_exempt
 def gitwebhooks(request):
     """
     For windows OS -- triggered when someone pushes to the repo on github -- TODO: implement HMAC hash verification
     """
+
+    proc = ''
     if request.method == 'POST':
-        try:
-            proc = subprocess.check_output([PULL_BATCH_FILE], cwd=PULL_BATCH_DIR, shell=True)
-        except Exception as e:
-            return HttpResponseServerError(e)
+        ref = request.get('ref')
 
-        # res, err = proc.communicate()
-
-        # if err:
-        #     return HttpResponseServerError(err)
+        if ref and ref == BRANCH_REF:
+            try:
+                proc = subprocess.check_output([PULL_BATCH_FILE], cwd=PULL_BATCH_DIR, shell=True)
+            except Exception as e:
+                return HttpResponseServerError(e)
 
     return HttpResponse(proc, status=200)
