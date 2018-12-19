ce6e96fd5129e6edbaab294941982759b6027bf5
Omar Younis <o.younis@pitechnologies.net>
2018-12-19 11:26:41 +0300

    add pull webhook

--- license_portal/helpers.py
+++ license_portal/helpers.py
@@ -1,24 +1,18 @@
 import subprocess
 import os
 
+from django.http.response import HttpResponseServerError, HttpResponse
+
 
 PULL_BATCH_PATH = os.path.join('C:\\', 'Apache24-general', 'htdocs', 'django_apps', 'license_portal', 'license_portal', 'gitpull_windows.bat')
-print(PULL_BATCH_PATH)
 
 
 def gitwebhooks(request):
-    proc = subprocess.Popen('', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen([PULL_BATCH_PATH], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     res, err = proc.communicate()
 
-    html = """
-        <h4>CMD out:</h4>
-        %(cmd_out)s
-        <hr>
-        <h4>CMD error:</h4>
-        %(cmd_err)s
-        """ % {'cmd_out': format_for_html(res.decode('utf-8')),
-               'cmd_err': None if not err else format_for_html(err.decode('utf-8'))}
+    if err:
+        return HttpResponseServerError(err)
 
-    # return HttpResponse(html)
-    print(html)
+    return HttpResponse(status=200)
