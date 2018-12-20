a4cf147ea62e254e749f24ea7e80c08a083db048
appdep <appdep@ubun175.stats.gov.sa>
2018-12-20 10:17:45 +0300

    updates from server

--- license_portal/wsgi.py
+++ license_portal/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 import sys
 
-sys.path.insert(len(sys.path) - 2, '/home/appdep/license_portal_env/lib/python3.7/site-packages')
+sys.path.insert(len(sys.path) - 2, '/home/appdep/license_portal_env/lib/python3.6/site-packages')
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(len(sys.path) - 2, path)
@@ -18,5 +18,7 @@ sys.path.insert(len(sys.path) - 2, path)
 from django.core.wsgi import get_wsgi_application
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "license_portal.settings"
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "license_portal.settings")
 
 application = get_wsgi_application()
